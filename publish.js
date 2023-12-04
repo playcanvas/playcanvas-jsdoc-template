15aa9f18368add94cd2189eb8de9b9371daad0ee
kungfooman <lama12345@gmail.com>
2023-12-04 18:03:06 +0100

    Support typedefs

--- publish.js
+++ publish.js
@@ -320,7 +320,7 @@ const typeLink = (type) => {
     const builtin = builtins[name.toLowerCase()];
     if (builtin) {
         url = builtin;
-    } else if (name.endsWith('Callback')) {
+    } else if (name.endsWith('Callback') || realTypedefs[type]) {
         url = `pc.html#${name}`;
     } else {
         url = clsUrl(name);
@@ -448,6 +448,14 @@ var setup = function (opts, callback) {
 };
 
 /**
+ * These are real @typedef's, neither callbacks nor import('abc').xyz
+ * This means they all have at least one property in `properties`.
+ *
+ * @type {Record<string, import('jsdoc').Doclet}
+ */
+const realTypedefs = {};
+
+/**
  * @param {TAFFY} taffyData - See <http://taffydb.com/>.
  * @param {object} opts - Options object.
  * @param {Tutorial} tutorials - Tutorials data.
@@ -472,6 +480,12 @@ exports.publish = (taffyData, opts, tutorials) => {
 
         // Strip all typedefs that are not callbacks
         data(function () {
+            if (this.kind === 'typedef' && !this.name.endsWith('Callback')) {
+                if (this.properties?.length) {
+                    realTypedefs[this.name] = this;
+                    return false;
+                }
+            }
             return this.kind === 'typedef' && !this.name.endsWith('Callback');
         }).remove();
 
